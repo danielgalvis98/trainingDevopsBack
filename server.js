9ac33620c6b6629bbdb899ecccdae59344aa1b59
Cristian Camilo David Martínez <cristian.david@eia.edu.co>
2020-04-06 09:28:54 -0500

    Change backend with the new database

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ var connection = mysql.createConnection({
 connection.connect();
 //Get movies from db
 function getMovies(callback) {    
-  connection.query("SELECT * FROM movie_db.publication",
+  connection.query("SELECT title, release, score, reviewer, publication FROM movie_db.publication WHERE pending = false",
+    function (err, rows) {
+      callback(err, rows); 
+    }
+  );
+}
+//Get pending movies from db
+function getPendingMovies(callback) {    
+  connection.query("SELECT title, release, score, reviewer, publication FROM movie_db.publication WHERE pending = true",
     function (err, rows) {
       callback(err, rows); 
     }
@@ -66,26 +74,21 @@ app.get('/publications', function(req, res, next) {
 });
 // Implement the movies API endpoint
 app.get('/movies', function(req, res){
-  var movies = [
-    {title : 'Suicide Squad', release: '2016', score: 8, reviewer: 'Robert Smith', publication : 'The Daily Reviewer'},    
-    {title : 'Batman vs. Superman', release : '2016', score: 6, reviewer: 'Chris Harris', publication : 'International Movie Critic'},
-    {title : 'Captain America: Civil War', release: '2016', score: 9, reviewer: 'Janet Garcia', publication : 'MoviesNow'},
-    {title : 'Deadpool', release: '2016', score: 9, reviewer: 'Andrew West', publication : 'MyNextReview'},
-    {title : 'Avengers: Age of Ultron', release : '2015', score: 7, reviewer: 'Mindy Lee', publication: 'Movies n\' Games'},
-    {title : 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},
-    {title : 'Guardians of the Galaxy', release : '2014', score: 10, reviewer: 'Anthony Miller', publication : 'ComicBookHero.com'},
-  ]
+  //now you can call the get-driver, passing a callback function
+  getMovies(function (err, moviesResult){ 
+    // you might want to do something is err is not null...      
+    res.json(moviesResult);
 
-  res.json(movies);
+  });
 })
 // Implement the pending reviews API endpoint
 app.get('/pending', function(req, res){
-  var pending = [
-    {title : 'Superman: Homecoming', release: '2017', score: 10, reviewer: 'Chris Harris', publication: 'International Movie Critic'},
-    {title : 'Wonder Woman', release: '2017', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},
-    {title : 'Doctor Strange', release : '2016', score: 7, reviewer: 'Anthony Miller', publication : 'ComicBookHero.com'}
-  ]
-  res.json(pending);
+  //now you can call the get-driver, passing a callback function
+  getPendingMovies(function (err, moviesResult){ 
+    // you might want to do something is err is not null...      
+    res.json(moviesResult);
+
+  });
 })
 console.log("server listening through port: "+process.env.PORT);
 // Launch our API Server and have it listen on port 3000.
