31ada483bcf85a5be1b1b80f365c767b9cf73c81
Cristian Camilo David Martínez <cristian.david@eia.edu.co>
2020-04-06 09:28:29 -0500

    Change structure of database, and adding some value

--- data_model/table_creation_and_inserts.sql
+++ data_model/table_creation_and_inserts.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE movie_db;
 CREATE TABLE movie_db.publication (name VARCHAR(255) PRIMARY KEY, avatar VARCHAR(21));
 CREATE TABLE movie_db.reviewer (name VARCHAR(255) PRIMARY KEY, avatar VARCHAR(255), publication VARCHAR(255), FOREIGN KEY (publication) REFERENCES publication(name) ON DELETE CASCADE);
-CREATE TABLE movie_db.moviereview (title VARCHAR(255) PRIMARY KEY, `release` VARCHAR(255), score INTEGER, reviewer VARCHAR(255), FOREIGN KEY (reviewer) REFERENCES reviewer(name) ON DELETE CASCADE);
+CREATE TABLE movie_db.moviereview (title VARCHAR(255) PRIMARY KEY, `release` VARCHAR(255), score INTEGER, reviewer VARCHAR(255), publication VARCHAR(255), pending BOOLEAN,FOREIGN KEY (reviewer) REFERENCES reviewer(name) ON DELETE CASCADE);
 
 INSERT INTO movie_db.publication (name, avatar) VALUES ('The Daily Reviewer', 'glyphicon-eye-open');
 INSERT INTO movie_db.publication (name, avatar) VALUES ('International Movie Critic', 'glyphicon-fire');
@@ -19,10 +19,13 @@ INSERT INTO movie_db.reviewer (name, avatar, publication) VALUES ('Mindy Lee','h
 INSERT INTO movie_db.reviewer (name, avatar, publication) VALUES ('Martin Thomas','https://s3.amazonaws.com/uifaces/faces/twitter/karsh/128.jpg','TheOne');
 INSERT INTO movie_db.reviewer (name, avatar, publication) VALUES ('Anthony Miller','https://s3.amazonaws.com/uifaces/faces/twitter/9lessons/128.jpg','ComicBookHero.com');
 
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Deadpool', '2016', 11, 'Robert Smith');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Thor: Ragnarok', '2017', 7, 'Chris Harris');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('It', '2017', 8, 'Janet Garcia');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Dunkirk', '2017', 8, 'Andrew West');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Logan', '2017', 8, 'Mindy Lee');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Batman V Superman', '2016', 6, 'Martin Thomas');
-INSERT INTO movie_db.moviereview (title, `release`, score, reviewer) VALUES ('Mad Max: Fury Road', '2015', 6, 'Anthony Miller');
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Deadpool', '2016', 11, 'Robert Smith', 'The Daily Reviewer', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Thor: Ragnarok', '2017', 7, 'Chris Harris', 'International Movie Critic', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('It', '2017', 8, 'Janet Garcia', 'MoviesNow', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Dunkirk', '2017', 8, 'Andrew West', 'MyNextReview', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Logan', '2017', 8, 'Mindy Lee', 'TheOne', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Batman V Superman', '2016', 6, 'Martin Thomas', 'Movies n\' Games', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Mad Max: Fury Road', '2015', 6, 'Anthony Miller', 'ComicBookHero.com', false);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Logan 2', '2020', 8, 'Mindy Lee', 'TheOne', true);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Batman V Superman 2', '2020', 6, 'Martin Thomas', 'Movies n\' Games', true);
+INSERT INTO movie_db.moviereview (title, `release`, score, reviewer, publication, pending) VALUES ('Mad Max: Fury Road 2', '2020', 6, 'Anthony Miller', 'ComicBookHero.com', true);
